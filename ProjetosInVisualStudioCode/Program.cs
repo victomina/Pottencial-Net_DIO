baf890a7652c5f98f96542a79917792e5ece74c8
victomina <victomina@gmail.com>
2022-12-05 02:18:44 -0300

    Exempode de fila

--- ProjetosInVisualStudioCode/Program.cs
+++ ProjetosInVisualStudioCode/Program.cs
@@ -6,6 +6,8 @@
 // Console.WriteLine("Minha vida a DEUS eu a dou!!");
 // Console.WriteLine("Compilando a minha vida!!");
 
+// Exemlo calculadora
+
 Calculadora calculadora = new Calculadora();
 
 calculadora.Somar(12,89);
@@ -13,3 +15,20 @@ calculadora.Somar(12,89);
 calculadora.Potencia(3,3);
 
 calculadora.Seno(60);
+ 
+ 
+ //Exemplo fila ---Queue
+
+ Queue<int> fila = new Queue<int>();
+ 
+ fila.Enqueue(2);
+ fila.Enqueue(4);
+ fila.Enqueue(6);
+ fila.Enqueue(8);
+
+ foreach(int item in fila)
+ {
+    Console.WriteLine(item);
+ }
+
+ 
