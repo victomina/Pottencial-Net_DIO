82b2e353de15a0f56c5359250cfa42d2de5cf35e
victomina <victomina@gmail.com>
2023-01-27 01:15:30 -0300

    Criando EndPoint de update

--- API .NET/Controllers/ContatoController.cs
+++ API .NET/Controllers/ContatoController.cs
@@ -34,5 +34,23 @@ namespace API_.NET.Controllers
 
             return Ok(contato);
         }
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if(contatoBanco == null)
+                return NotFound();
+            
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
+
     }
 }
