daf3c23a8d572813ca0b0496047d95375ea27f7c
victomina <victomina@gmail.com>
2023-03-14 00:34:14 -0300

    Configurando método na controller

--- Projeto MVC/Controllers/ContatoController.cs
+++ Projeto MVC/Controllers/ContatoController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Projeto_MVC.Context;
 
 namespace Projeto_MVC.Controllers
 {
     public class ContatoController : Controller
     {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
         }
     }
 }
