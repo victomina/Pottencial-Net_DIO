b2a0381ca65f23690dc41096d48605cb62d1c985
victomina <victomina@gmail.com>
2023-03-16 01:58:38 -0300

    Criando método de Exibir Detalhes do contato

--- Projeto MVC/Controllers/ContatoController.cs
+++ Projeto MVC/Controllers/ContatoController.cs
@@ -63,5 +63,14 @@ namespace Projeto_MVC.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if(contato == null)
+                return RedirectToAction(nameof(Index));
+            return View(contato);
+        }
+
     }
 }
