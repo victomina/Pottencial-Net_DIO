d163114c6b3493956b84bd506cb83ab8ea20c06f
victomina <victomina@gmail.com>
2023-03-15 23:46:42 -0300

    build e refatoração

--- Projeto MVC/Controllers/ContatoController.cs
+++ Projeto MVC/Controllers/ContatoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Projeto_MVC.Context;
+using Projeto_MVC.Models;
 
 namespace Projeto_MVC.Controllers
 {
@@ -24,6 +25,18 @@ namespace Projeto_MVC.Controllers
         public IActionResult Criar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
         }
     }
 }
